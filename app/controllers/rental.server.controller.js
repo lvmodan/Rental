00a547ebdba22656ffb91d19c0e33d764487033f
fire <AmyGitHub@126.com>
2017-04-15 00:08:05 +0800

    change the url

--- app/controllers/rental.server.controller.js
+++ app/controllers/rental.server.controller.js
@@ -3,7 +3,6 @@ var cheerio = require('cheerio');
 var cfg = require('../../config/config')
 
 /*url存储对象*/
-<detail>
 let rentalObj = (function(){
 	/*保存58同城上爬取的每个租房的URL*/
 	let rentalSet =  new Set();
@@ -27,7 +26,7 @@ let rentalObj = (function(){
 		}
 	}
 })();
-</detail>
+
 /*url读取解析对象*/
 let rentalInfosObj = (function(){
 	/*二手房解析出来的数据存储在该map中*/
@@ -107,7 +106,8 @@ let rentalInfosObj = (function(){
 *函数返回值 ：url
 ***********************************************************************************************/
 function getUrl(page = 1){
-	return  'http://bj.58.com/chuzu/pn'+page+'/?key=%E6%9D%AD%E5%B7%9E%E7%A7%9F%E6%88%BF%E5%AD%90&cmcskey=%E7%A7%9F%E6%88%BF%E5%AD%90&final=1&PGTID=0d3090a7-0004-f43c-ee04-95c2ea3d031f&ClickID=6';
+	//return  'http://bj.58.com/chuzu/pn'+page+'/?key=%E6%9D%AD%E5%B7%9E%E7%A7%9F%E6%88%BF%E5%AD%90&cmcskey=%E7%A7%9F%E6%88%BF%E5%AD%90&final=1&PGTID=0d3090a7-0004-f43c-ee04-95c2ea3d031f&ClickID=6';
+	return 'http://bj.58.com/shangdi/chuzu/b10/pn'+page+'/?PGTID=0d3090a7-004d-616f-e609-bfd6370d1c4d&ClickID=3';
 }
 
 /***********************************************************************************************
